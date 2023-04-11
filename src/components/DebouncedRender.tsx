c3ef0ed7dfca40df2320a417d750cdd35708f670
doganhekimoglu <doganhekimoglu@outlook.com>
2023-04-11 18:55:49 +0300

    tests updated

--- src/components/DebouncedRender.tsx
+++ src/components/DebouncedRender.tsx
@@ -24,6 +24,7 @@ const DebouncedRender: React.FC<DebouncedRenderProps> = ({
 
   const minDurationTimer = useRef<number>(0);
   const debounceTimer = useRef<number>(0);
+  const mounted = useRef<boolean>(false);
 
   const [debounceTime] = useState<number>(delay || 0);
   const [hideDuration] = useState<number>(minDuration || 0);
@@ -31,16 +32,21 @@ const DebouncedRender: React.FC<DebouncedRenderProps> = ({
   const [render, setRender] = useState(debounceTime === 0 && renderCondition);
 
   useEffect(() => {
+    mounted.current = true;
     if (!renderCondition && !render) {
       window.clearTimeout(minDurationTimer.current);
       window.clearTimeout(debounceTimer.current);
       setMinDurationPassed(false);
       setDebounceTimePassed(false);
     }
+    return () => {
+      mounted.current = false;
+    };
   }, [renderCondition, render]);
 
   // Invoke related callbacks if they are provided
   useEffect(() => {
+    mounted.current = true;
     if (render) {
       setRenderedOnce(true);
       if (onRender) {
@@ -49,10 +55,14 @@ const DebouncedRender: React.FC<DebouncedRenderProps> = ({
     } else if (renderedOnce && onHide) {
       onHide();
     }
+    return () => {
+      mounted.current = false;
+    };
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [render]);
 
   useEffect(() => {
+    mounted.current = true;
     if (
       // If children is already shown, wanted to be hidden and minDurationPassed, hide it
       (render && !renderCondition && ((minDurationPassed && hideDuration) || !hideDuration)) ||
@@ -61,28 +71,39 @@ const DebouncedRender: React.FC<DebouncedRenderProps> = ({
     ) {
       setRender(renderCondition);
     }
+    return () => {
+      mounted.current = false;
+    };
   }, [render, renderCondition, minDurationPassed, debounceTimePassed, hideDuration, debounceTime]);
 
   useEffect(() => {
+    mounted.current = true;
     // Start timer to measure min duration time after the children is mounted
     if (render && hideDuration) {
       window.clearTimeout(minDurationTimer.current);
       setMinDurationPassed(false);
       minDurationTimer.current = window.setTimeout(() => {
-        setMinDurationPassed(true);
+        if (mounted.current) setMinDurationPassed(true);
       }, hideDuration);
     }
+    return () => {
+      mounted.current = false;
+    };
   }, [render, hideDuration]);
 
   useEffect(() => {
+    mounted.current = true;
     // Start timer to measure debounce time after renderCondition is set to true while render is false
     if (renderCondition && debounceTime && !render) {
       window.clearTimeout(debounceTimer.current);
       setDebounceTimePassed(false);
       debounceTimer.current = window.setTimeout(() => {
-        setDebounceTimePassed(true);
+        if (mounted.current) setDebounceTimePassed(true);
       }, debounceTime);
     }
+    return () => {
+      mounted.current = false;
+    };
   }, [renderCondition, debounceTime, render]);
 
   return <React.Fragment>{render ? children : null}</React.Fragment>;
--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,56 +3,97 @@ import * as React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 
 import { DebouncedRender } from '../src';
+import { useMemo } from 'react';
 
 const wait = (ms: number) => new Promise((resolve) => window.setTimeout(resolve, ms));
 
+function Wrapper({ children, show }: { children: JSX.Element; show: boolean }) {
+  return useMemo(() => {
+    if (show) return children;
+    else return <></>;
+  }, [children, show]);
+}
+
 test('Children should be rendered after debounce delay passes', async () => {
+  const debounceDelay = 1000;
   await waitFor(
     async () => {
-      const debounceDelay = 1000;
-
-      //await act(() => {
       render(
         <DebouncedRender delay={debounceDelay} renderCondition={true}>
           loading
         </DebouncedRender>,
       );
-      //});
 
       await wait(debounceDelay + 50);
-
-      const linkElement = screen.queryByText('loading');
-      expect(linkElement).toBeInTheDocument();
     },
     { timeout: 100000 },
   );
+  const el = screen.queryByText('loading');
+  expect(el).toBeInTheDocument();
 });
 
-test('Children should not be rendered before debounce delay passes', async () => {
+test('Debounce delay timer should be reset if the component unmounted and mounted', async () => {
+  const debounceDelay = 1000;
   await waitFor(
     async () => {
-      const debounceDelay = 1000;
-
-      render(
-        <DebouncedRender delay={debounceDelay} renderCondition={true}>
-          loading
-        </DebouncedRender>,
+      const { rerender } = render(
+        <Wrapper show={true}>
+          <DebouncedRender delay={debounceDelay} renderCondition={true}>
+            loading
+          </DebouncedRender>
+        </Wrapper>,
       );
 
       await wait(debounceDelay - 50);
 
-      const linkElement = screen.queryByText('loading');
-      expect(linkElement).not.toBeInTheDocument();
+      rerender(
+        <Wrapper show={false}>
+          <DebouncedRender delay={debounceDelay} renderCondition={true}>
+            loading
+          </DebouncedRender>
+        </Wrapper>,
+      );
+
+      rerender(
+        <Wrapper show={true}>
+          <DebouncedRender delay={debounceDelay} renderCondition={true}>
+            loading
+          </DebouncedRender>
+        </Wrapper>,
+      );
+
+      await wait(100);
     },
     { timeout: 100000 },
   );
+  const el = screen.queryByText('loading');
+  expect(el).not.toBeInTheDocument();
+});
+
+test('Children should not be rendered before debounce delay passes', async () => {
+  const debounceDelay = 1000;
+  await waitFor(
+    async () => {
+      render(
+        <Wrapper show={true}>
+          <DebouncedRender delay={debounceDelay} renderCondition={true}>
+            loading
+          </DebouncedRender>
+        </Wrapper>,
+      );
+
+      await wait(debounceDelay - 50);
+    },
+    { timeout: 100000 },
+  );
+  const el = screen.queryByText('loading');
+  expect(el).not.toBeInTheDocument();
 });
 
 test('Children should be unmounted when renderCondition is set to false and after minDuration passes', async () => {
+  const minDuration = 1000;
   await waitFor(
     async () => {
-      const minDuration = 1000;
-
       const { rerender } = render(
         <DebouncedRender renderCondition={true} minDuration={minDuration}>
           loading
@@ -67,20 +108,18 @@ test('Children should be unmounted when renderCondition is set to false and afte
       );
 
       await wait(100);
-
-      const linkElement = screen.queryByText('loading');
-      expect(linkElement).not.toBeInTheDocument();
     },
     { timeout: 100000 },
   );
+  const el = screen.queryByText('loading');
+  expect(el).not.toBeInTheDocument();
 });
 
 test('Children should not be unmounted when renderCondition is set to false and before minDuration passes', async () => {
+  const debounceDelay = 0;
+  const minDuration = 500;
   await waitFor(
     async () => {
-      const debounceDelay = 0;
-      const minDuration = 500;
-
       const { rerender } = render(
         <DebouncedRender delay={debounceDelay} renderCondition={true} minDuration={minDuration}>
           loading
@@ -96,23 +135,20 @@ test('Children should not be unmounted when renderCondition is set to false and 
       );
 
       await wait(50);
-
-      const linkElement = screen.queryByText('loading');
-      expect(linkElement).toBeInTheDocument();
     },
     { timeout: 100000 },
   );
+  const el = screen.queryByText('loading');
+  expect(el).toBeInTheDocument();
 });
 
 test('onRender callback should be called after the children is mounted', async () => {
+  const debounceDelay = 1000;
+  const renderCallback = jest.fn(() => {
+    return;
+  });
   await waitFor(
     async () => {
-      const debounceDelay = 1000;
-
-      const renderCallback = jest.fn(() => {
-        return;
-      });
-
       render(
         <DebouncedRender delay={debounceDelay} renderCondition={true} onRender={renderCallback}>
           loading
@@ -120,22 +156,19 @@ test('onRender callback should be called after the children is mounted', async (
       );
 
       await wait(debounceDelay + 50);
-
-      expect(renderCallback).toHaveBeenCalledTimes(1);
     },
     { timeout: 100000 },
   );
+  expect(renderCallback).toHaveBeenCalledTimes(1);
 });
 
 test('onRender callback should not be called before the children is mounted', async () => {
+  const debounceDelay = 1000;
+  const renderCallback = jest.fn(() => {
+    return;
+  });
   await waitFor(
     async () => {
-      const debounceDelay = 1000;
-
-      const renderCallback = jest.fn(() => {
-        return;
-      });
-
       render(
         <DebouncedRender delay={debounceDelay} renderCondition={true} onRender={renderCallback}>
           loading
@@ -143,23 +176,21 @@ test('onRender callback should not be called before the children is mounted', as
       );
 
       await wait(debounceDelay - 50);
-
-      expect(renderCallback).not.toHaveBeenCalled();
     },
     { timeout: 100000 },
   );
+  expect(renderCallback).not.toHaveBeenCalled();
 });
 
 test('onHide callback should be called after the children is unmounted', async () => {
+  const debounceDelay = 0;
+  const minDuration = 1000;
+
+  const hideCallback = jest.fn(() => {
+    return;
+  });
   await waitFor(
     async () => {
-      const debounceDelay = 0;
-      const minDuration = 1000;
-
-      const hideCallback = jest.fn(() => {
-        return;
-      });
-
       const { rerender } = render(
         <DebouncedRender delay={debounceDelay} renderCondition={true} minDuration={minDuration} onHide={hideCallback}>
           loading
@@ -175,23 +206,21 @@ test('onHide callback should be called after the children is unmounted', async (
       );
 
       await wait(100);
-
-      expect(hideCallback).toHaveBeenCalledTimes(1);
     },
     { timeout: 100000 },
   );
+  expect(hideCallback).toHaveBeenCalledTimes(1);
 });
 
 test('onHide callback should not be called before the children is unmounted', async () => {
+  const debounceDelay = 0;
+  const minDuration = 1000;
+
+  const hideCallback = jest.fn(() => {
+    return;
+  });
   await waitFor(
     async () => {
-      const debounceDelay = 0;
-      const minDuration = 1000;
-
-      const hideCallback = jest.fn(() => {
-        return;
-      });
-
       const { rerender } = render(
         <DebouncedRender delay={debounceDelay} renderCondition={true} minDuration={minDuration} onHide={hideCallback}>
           loading
@@ -207,9 +236,8 @@ test('onHide callback should not be called before the children is unmounted', as
       );
 
       await wait(50);
-
-      expect(hideCallback).not.toHaveBeenCalled();
     },
     { timeout: 100000 },
   );
+  expect(hideCallback).not.toHaveBeenCalled();
 });
